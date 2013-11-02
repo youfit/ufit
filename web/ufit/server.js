b99d2d12a007cbdf2b70383354869572c282622b
Michael Dobson <mi.j.dobson@gmail.com>
2013-11-02 17:33:39 -0500

    wkout selection view

--- web/ufit/server.js
+++ web/ufit/server.js
@@ -1,16 +1,11 @@
 var express =		require('express'),
 	fs =	 		require('fs'),
 	path = 			require('path'),
-	//workout_list = 	require(path.join(process.cwd(), '/data/workout.json'));
 	workout_list = 	require('./data/workout');
 
 var app = express.createServer();	
 	
 app.get('/', function (req, res) {
-	res.end("uFit");
-});
-
-app.get('/preferences', function (req, res) {
 	res.render('preferences.ejs', {title: 'Perferences'});
 });
 
@@ -18,4 +13,7 @@ app.get('/select', function (req, res) {
 	res.render('select.ejs', {title: 'Select'});
 });
 
-app.listen(3000);
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+  });
