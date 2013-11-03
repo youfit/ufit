a5c438b21c7a727143e08ed114cd5f16ec902e8e
Michael Dobson <mi.j.dobson@gmail.com>
2013-11-03 00:23:17 -0500

    preference and search pages

--- web/ufit/server.js
+++ web/ufit/server.js
@@ -4,7 +4,8 @@ var express =		require('express'),
 	workout_list = 	require('./data/workout');
 
 var app = express.createServer();	
-	
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', function (req, res) {
 	res.render('preferences.ejs', {title: 'Perferences'});
 });
